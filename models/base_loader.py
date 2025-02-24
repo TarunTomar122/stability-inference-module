16c1af435f47e99e5ef664ffede3939b6b785de3
TarunAdobe <ttomar@adobe.com>
2025-02-24 19:05:06 +0530

    removed models

--- models/base_loader.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-from typing import Optional, Union
-
-from utils.device_utils import move_to_device, get_device, get_dtype
-from configs.model_config import ModelConfig
-
-import torch
-from safetensors.torch import load_file as load_safetensors 
